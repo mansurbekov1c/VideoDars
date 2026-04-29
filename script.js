a7b90d81fbeee03c3db5622453318fd637020628
mansurbekov1c <abdullayevjorabek1pc@gmail.com>
2026-04-29 23:18:04 +0500

    5-dars

---
+++ script.js
@@ -0,0 +1,61 @@
+// 5-dars
+// Window module
+
+// alert("Are you sure?");
+
+// let isAccess = confirm("Are you above 18 age?");
+// console.log(isAccess);
+
+// let fName = prompt("What is your name?");
+// console.log(fName);
+
+// let age = +prompt("How old are you", "15");
+// console.log(age);
+
+// Interpolatsiya
+
+// let fName = "Jurabek";
+// let lName = "Abdullayev";
+// let age = 16;
+
+// console.log(`My full name is ${fName} ${lName} , I'm ${age} years old.`);
+
+// let username = "_mansurbekov1c";
+
+// console.log(`https://instagram.com/@${username}`);
+
+// Operatorlar
+// +, -, /, *, %
+
+// Increment xar doim sonni bittaga oshiradi
+// Decrement xar doim sonni bittaga kamaytiradi
+
+// let x = 6;
+// let y = 6;
+
+// // postfix
+// // x++;
+// // y--;
+
+// // prefix
+// // ++x;
+// // --y
+
+// // console.log da prefix ishlaydi (o'zgaruvchidan oldin qo'yilgani uchun)
+// console.log(++x);
+// console.log(--y);
+
+// && va - hamma malumot true bo'lsa true qaytaradi
+// || yoki - bitta malumot true bo'lsa true qaytaradi
+
+// let isMarried = true;
+// let isHaveChild = false;
+
+// let family = isMarried || isHaveChild;
+
+// console.log(family);
+
+// Amaliyot
+
+// let age = +prompt("How old are you?");
+// console.log(`I'm ${age} years old`);
