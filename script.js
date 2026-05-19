24de199e218eb904abd7fe45673d55a62b2b346a
mansurbekov1c <abdullayevjorabek1pc@gmail.com>
2026-05-19 14:55:33 +0500

    12-dars. Callback va Object destruktizatsiya

--- script.js
+++ script.js
@@ -278,68 +278,130 @@
 //
 // Homework
 
-let numberOfSeries;
+// let numberOfSeries;
 
-startApp();
+// startApp();
 
-const seriesDB = {
-  count: numberOfSeries,
-  series: {},
-  actors: {},
-  genres: [],
-  private: false,
+// const seriesDB = {
+//   count: numberOfSeries,
+//   series: {},
+//   actors: {},
+//   genres: [],
+//   private: false,
+// };
+
+// setFavouriteSeries();
+// detectingLevel();
+// (writeGenres(), showDb(seriesDB.private));
+
+// function startApp() {
+//   numberOfSeries = +prompt("Nechta serial ko'rdingiz?");
+
+//   while (
+//     numberOfSeries == "" ||
+//     numberOfSeries == null ||
+//     isNaN(numberOfSeries)
+//   ) {
+//     numberOfSeries = +prompt("Nechta serial ko'rdingiz?");
+//   }
+// }
+
+// function setFavouriteSeries() {
+//   for (let i = 0; i < 2; i++) {
+//     const a = prompt(`Oxirgi ko'rgan serialingiz ${i + 1}`);
+//     const b = prompt(`Nechi baxo berasiz? ${i + 1}`);
+
+//     if (a !== null && b !== null && a !== "" && b !== "") {
+//       seriesDB.series[a] = b;
+//     } else {
+//       i--;
+//     }
+//   }
+// }
+
+// function detectingLevel() {
+//   if (seriesDB.count < 5) {
+//     console.log("Kam serial ko'ribsiz");
+//   } else if (seriesDB.count >= 5 && seriesDB.count < 10) {
+//     console.log("Siz classic tomoshabin ekansiz");
+//   } else if (seriesDB.count >= 10) {
+//     console.log("Siz g'irt bekorchi ekansiz");
+//   }
+// }
+
+// function writeGenres() {
+//   for (let i = 0; i < 3; i++) {
+//     const genre = prompt(`Yaxshi ko'rgan janringiz ${i + 1}?`);
+//     seriesDB.genres[i] = genre;
+//   }
+// }
+
+// function showDb(isPrivate) {
+//   if (!isPrivate) {
+//     console.log(seriesDB);
+//   } else {
+//     console.log("Ma'lumotlar mahfiy saqlanmoqda");
+//   }
+// }
+
+//
+// 12-dars. Callback va Object destruktizatsiya
+//
+
+// Callback bu bitta function bajarilgandan so'ng ishlaydigan function
+
+// function greeting(callback) {
+//   console.log("Hello students");
+
+//   callback();
+// }
+
+// function callback() {
+//   console.log("Are you ready today");
+// }
+
+// greeting(callback);
+
+// Object va destruktizatsiya
+
+const person = {
+  name: "Samar",
+  year: 2001,
+  job: "Full Stack developer",
+  hobbies: {
+    sport: "Football",
+    games: "UFC",
+  },
+  sayHello: function () {
+    console.log(`hello ${this.name}`);
+  },
 };
 
-setFavouriteSeries();
-detectingLevel();
-(writeGenres(), showDb(seriesDB.private));
+console.log(person);
 
-function startApp() {
-  numberOfSeries = +prompt("Nechta serial ko'rdingiz?");
+person.sayHello();
 
-  while (
-    numberOfSeries == "" ||
-    numberOfSeries == null ||
-    isNaN(numberOfSeries)
-  ) {
-    numberOfSeries = +prompt("Nechta serial ko'rdingiz?");
-  }
-}
+const {
+  job,
+  year,
+  name,
+  sayHello,
+  hobbies: { games, sport },
+} = person;
 
-function setFavouriteSeries() {
-  for (let i = 0; i < 2; i++) {
-    const a = prompt(`Oxirgi ko'rgan serialingiz ${i + 1}`);
-    const b = prompt(`Nechi baxo berasiz? ${i + 1}`);
+console.log(games);
 
-    if (a !== null && b !== null && a !== "" && b !== "") {
-      seriesDB.series[a] = b;
-    } else {
-      i--;
-    }
-  }
-}
+// console.log(Object.keys(person).length);
 
-function detectingLevel() {
-  if (seriesDB.count < 5) {
-    console.log("Kam serial ko'ribsiz");
-  } else if (seriesDB.count >= 5 && seriesDB.count < 10) {
-    console.log("Siz classic tomoshabin ekansiz");
-  } else if (seriesDB.count >= 10) {
-    console.log("Siz g'irt bekorchi ekansiz");
-  }
-}
+// for in - faqat object uchun
+// for of - faqat massiv ya'ni array uchun
 
-function writeGenres() {
-  for (let i = 0; i < 3; i++) {
-    const genre = prompt(`Yaxshi ko'rgan janringiz ${i + 1}?`);
-    seriesDB.genres[i] = genre;
-  }
-}
-
-function showDb(isPrivate) {
-  if (!isPrivate) {
-    console.log(seriesDB);
-  } else {
-    console.log("Ma'lumotlar mahfiy saqlanmoqda");
-  }
-}
+// for (let key in person) {
+//   if (typeof person[key] === "object") {
+//     for (let i in person[key]) {
+//       console.log(`Property: ${i}: Value: ${person[key][i]}`);
+//     }
+//   } else {
+//     console.log(`Property: ${key}: Value: ${person[key]}`);
+//   }
+// }
